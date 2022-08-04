b6a5f43ea1abbaa2a747a78a95c52b072f005b50
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-05 00:57:33 +0300

    add Task1-8

---
+++ Task1-8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принемает число (N),
+//а на выходе показывает все четные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+if(count < N)
+{
+    while(count<=N)
+    {
+        Console.WriteLine(count);
+        count+=2;
+    }
+}
+else
+    {
+        Console.WriteLine("Введите целое число больше 2");
+    }
