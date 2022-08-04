c0e054a7d7974928a64f9402fc857ed6086de4bd
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-04 19:39:12 +0300

    add Task1-6

---
+++ Task1-6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принемает число и выдает,
+// является ли число четным (делится ли оно на 2 без остатка).
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N%2==0) 
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+   Console.WriteLine("НЕТ"); 
+}
